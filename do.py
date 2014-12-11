29a38e8afd2d36fba62fe6ef95d74201d9efa357
Nicolas Herbaut <nherbaut@enseirb-matmeca.fr>
2014-12-11 11:48:07 +0100

    python egg

---
+++ do.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from adaptation.commons import *
+
+ddo("http://192.168.2.122/vid.mp4")
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='vhg-adaptation-worker',
+    version='0.1',
+    description='Worker for video adaptation',
+    author='Nicolas Herbaut',
+    author_email='nherbaut@labri.fr',
+    packages=['adaptation'])
