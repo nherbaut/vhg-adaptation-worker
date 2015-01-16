1dd8913620fc557ab7c3805c6a6ebbdf479bb9eb
Charles-Damien PAPOT <chadapap@gmail.com>
2015-01-16 16:01:08 +0100

    fix encoding

--- adaptation/settings2.py
+++ adaptation/settings2.py
@@ -2,5 +2,5 @@ BROKER_URL = 'amqp://guest:guest@localhost'
 CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT = ['json']
-config = {"folder_out": "/var/www/html", "bitrates_size_dict": {240:20, 480:1500, 720:3000}}
+#CELERY_ACCEPT_CONTENT = ['json']
+config = {"folder_out": "/home/user/output", "bitrates_size_dict": {100: 100, 200:200}}
