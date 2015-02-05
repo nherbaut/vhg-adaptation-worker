8932637c8d7f5e15a6871d6ccb38ce013a5364ba
Nicolas Herbaut <nherbaut@enseirb-matmeca.fr>
2015-02-05 11:20:24 +0100

    moved files to doc

--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-celery -A adaptation.commons worker --loglevel=info --concurrency=1
--- start_background.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-celery -A adaptation.commons worker --loglevel=info --concurrency=1 > logs.txt 2>&1 &
